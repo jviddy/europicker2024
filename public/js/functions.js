c0fb4963de10629303cd0f7500a3465f1bad447d
Jamie Vidamour <jamie@vidamour.net>
2024-04-13 10:06:14 +0100

    Working with updated teamList and using find inside of team list instead of usingt he cost as an index. Team list no longer needs to be sorted

--- public/js/functions.js
+++ public/js/functions.js
@@ -7,31 +7,31 @@ document.addEventListener("DOMContentLoaded", function() {
     const maxTeamCount = 3;
 
     let teamList = [
-        {"countryName":"0","groupLetter":"0","groupPos":"0","fifaRank":0,"cost":0},
-        {"countryName":"Georgia","groupLetter":"F","groupPos":"2","fifaRank":75,"cost":1},
-        {"countryName":"Albania","groupLetter":"B","groupPos":"4","fifaRank":66,"cost":2},
-        {"countryName":"Slovenia","groupLetter":"C","groupPos":"1","fifaRank":57,"cost":3},
-        {"countryName":"Slovakia","groupLetter":"E","groupPos":"2","fifaRank":48,"cost":4},
-        {"countryName":"Romania","groupLetter":"E","groupPos":"3","fifaRank":46,"cost":5},
-        {"countryName":"Turkey","groupLetter":"F","groupPos":"1","fifaRank":40,"cost":6},
-        {"countryName":"Scotland","groupLetter":"A","groupPos":"2","fifaRank":39,"cost":7},
-        {"countryName":"Czech Republic","groupLetter":"F","groupPos":"4","fifaRank":36,"cost":8},
-        {"countryName":"Serbia","groupLetter":"C","groupPos":"3","fifaRank":33,"cost":9},
-        {"countryName":"Poland","groupLetter":"D","groupPos":"1","fifaRank":28,"cost":10},
-        {"countryName":"Hungary","groupLetter":"A","groupPos":"3","fifaRank":26,"cost":11},
-        {"countryName":"Austria","groupLetter":"D","groupPos":"3","fifaRank":25,"cost":12},
-        {"countryName":"Ukraine","groupLetter":"E","groupPos":"4","fifaRank":22,"cost":13},
-        {"countryName":"Denmark","groupLetter":"C","groupPos":"2","fifaRank":21,"cost":14},
-        {"countryName":"Switzerland","groupLetter":"A","groupPos":"4","fifaRank":19,"cost":15},
-        {"countryName":"Germany","groupLetter":"A","groupPos":"1","fifaRank":16,"cost":16},
-        {"countryName":"Croatia","groupLetter":"B","groupPos":"2","fifaRank":10,"cost":17},
-        {"countryName":"Italy","groupLetter":"B","groupPos":"3","fifaRank":9,"cost":18},
-        {"countryName":"Spain","groupLetter":"B","groupPos":"1","fifaRank":8,"cost":19},
-        {"countryName":"Netherlands","groupLetter":"D","groupPos":"2","fifaRank":7,"cost":20},
-        {"countryName":"Portugal","groupLetter":"F","groupPos":"3","fifaRank":6,"cost":21},
-        {"countryName":"England","groupLetter":"C","groupPos":"4","fifaRank":4,"cost":22},
-        {"countryName":"Belgium","groupLetter":"E","groupPos":"1","fifaRank":3,"cost":23},
-        {"countryName":"France","groupLetter":"D","groupPos":"4","fifaRank":2,"cost":24}
+        {"countryId":"0","countryName":"0","groupLetter":"0","groupPos":"0","fifaRank":0,"cost":0},
+        {"countryId":"F2","countryName":"Georgia","groupLetter":"F","groupPos":"2","fifaRank":75,"cost":1},
+        {"countryId":"B4","countryName":"Albania","groupLetter":"B","groupPos":"4","fifaRank":66,"cost":2},
+        {"countryId":"C1","countryName":"Slovenia","groupLetter":"C","groupPos":"1","fifaRank":57,"cost":3},
+        {"countryId":"E2","countryName":"Slovakia","groupLetter":"E","groupPos":"2","fifaRank":48,"cost":4},
+        {"countryId":"E3","countryName":"Romania","groupLetter":"E","groupPos":"3","fifaRank":46,"cost":5},
+        {"countryId":"F1","countryName":"Turkey","groupLetter":"F","groupPos":"1","fifaRank":40,"cost":6},
+        {"countryId":"A2","countryName":"Scotland","groupLetter":"A","groupPos":"2","fifaRank":39,"cost":7},
+        {"countryId":"F4","countryName":"Czech Republic","groupLetter":"F","groupPos":"4","fifaRank":36,"cost":8},
+        {"countryId":"C3","countryName":"Serbia","groupLetter":"C","groupPos":"3","fifaRank":33,"cost":9},
+        {"countryId":"D1","countryName":"Poland","groupLetter":"D","groupPos":"1","fifaRank":28,"cost":10},
+        {"countryId":"A3","countryName":"Hungary","groupLetter":"A","groupPos":"3","fifaRank":26,"cost":11},
+        {"countryId":"D3","countryName":"Austria","groupLetter":"D","groupPos":"3","fifaRank":25,"cost":12},
+        {"countryId":"E4","countryName":"Ukraine","groupLetter":"E","groupPos":"4","fifaRank":22,"cost":13},
+        {"countryId":"C2","countryName":"Denmark","groupLetter":"C","groupPos":"2","fifaRank":21,"cost":14},
+        {"countryId":"A4","countryName":"Switzerland","groupLetter":"A","groupPos":"4","fifaRank":19,"cost":15},
+        {"countryId":"A1","countryName":"Germany","groupLetter":"A","groupPos":"1","fifaRank":16,"cost":16},
+        {"countryId":"B2","countryName":"Croatia","groupLetter":"B","groupPos":"2","fifaRank":10,"cost":17},
+        {"countryId":"B3","countryName":"Italy","groupLetter":"B","groupPos":"3","fifaRank":9,"cost":18},
+        {"countryId":"B1","countryName":"Spain","groupLetter":"B","groupPos":"1","fifaRank":8,"cost":19},
+        {"countryId":"D2","countryName":"Netherlands","groupLetter":"D","groupPos":"2","fifaRank":7,"cost":20},
+        {"countryId":"F3","countryName":"Portugal","groupLetter":"F","groupPos":"3","fifaRank":6,"cost":21},
+        {"countryId":"C4","countryName":"England","groupLetter":"C","groupPos":"4","fifaRank":4,"cost":22},
+        {"countryId":"E1","countryName":"Belgium","groupLetter":"E","groupPos":"1","fifaRank":3,"cost":23},
+        {"countryId":"D4","countryName":"France","groupLetter":"D","groupPos":"4","fifaRank":2,"cost":24},
     ]
 
     for (var i = 0; i < tables.length; i++) {
@@ -108,12 +108,13 @@ document.addEventListener("DOMContentLoaded", function() {
 
             // Populate table with selected teams
             for (var t = 0; t < selectedTeams.length; t++) {
-                var selectedTeam = parseInt(selectedTeams[t]);
-                    var tcost = teamList[selectedTeam].cost;
+                console.log(selectedTeams[t])
+                var selectedTeam = teamList.find(country => country.countryId === selectedTeams[t]);
+                    var tcost = selectedTeam.cost;
                     console.log (tcost);
                     var row = table.insertRow();
                     //console.log(tCost);
-                    row.setAttribute('id', tcost);
+                    row.setAttribute('id', selectedTeams[t]);
                     row.addEventListener("click", function() {
                         addRemoveTeam(this.id);
                         var teamRow = document.getElementById(this.id);
@@ -121,10 +122,10 @@ document.addEventListener("DOMContentLoaded", function() {
                     });
                     
                     var countryCell = row.insertCell();
-                    countryCell.textContent = teamList[selectedTeam].countryName;
+                    countryCell.textContent = selectedTeam.countryName;
                     
                     var costCell = row.insertCell();
-                    costCell.textContent = teamList[selectedTeam].cost;
+                    costCell.textContent = selectedTeam.cost;
                 }
             
                 var totalRow = table.insertRow();
